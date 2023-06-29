81b04f41c19c5495d7bfb82643d0d6a0842caaaf
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2023-06-29 16:45:31 +0700

    Add SignIn in auth_controller

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,6 +1,10 @@
 import 'package:aplikasipendaftaranklinik/model/user_model.dart';
+import 'package:aplikasipendaftaranklinik/themes/custom_colors.dart';
+import 'package:aplikasipendaftaranklinik/utils/constants.dart';
+import 'package:aplikasipendaftaranklinik/view/register.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthController {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -37,6 +41,120 @@ class AuthController {
       }
     } catch (e) {
       print('Error signin in: $e');
+    }
+    return null;
+  }
+
+  void showAlertUserNotFound(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      title: const Text(titleError),
+      content: const Text("Maaf, User Tidak Ditemukan!"),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text(
+            "OK",
+            style: TextStyle(color: colorPinkText),
+          ),
+        ),
+      ],
+    );
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  void showAlertUserWrongPassword(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      title: const Text(titleError),
+      content: const Text("Maaf, Password Salah!"),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text(
+            "OK",
+            style: TextStyle(color: colorPinkText),
+          ),
+        ),
+      ],
+    );
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  void showAlertDialogLoading(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      content: Row(
+        children: [
+          const CircularProgressIndicator(),
+          Container(
+            margin: const EdgeInsets.only(left: 15),
+            child: const Text(
+              "Loading...",
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+        ],
+      ),
+    );
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  Future<UserModel?> signInWithEmailAndPassword(
+      String email, String password, BuildContext context) async {
+    showAlertDialogLoading(context);
+    try {
+      final UserCredential userCredential = //untuk
+          await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final User? user = userCredential.user; //untuk
+
+      if (user != null) {
+        final DocumentSnapshot snapshot =
+            await userCollection.doc(user.uid).get();
+        final UserModel currentUser = UserModel(
+          uId: user.uid,
+          nama: snapshot['nama'],
+          email: user.email ?? '',
+          role: snapshot['role'],
+          alamat: snapshot['alamat'],
+          jekel: snapshot['jekel'],
+          nomorhp: snapshot['nomorhp'],
+          tglLahir: snapshot['tglLahir'],
+          noAntrian: snapshot['noAntrian'],
+          poli: snapshot['poli'],
+        );
+        // ignore: use_build_context_synchronously
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const Register()),
+          (route) => false,
+        );
+        return currentUser;
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        showAlertUserNotFound(context);
+      } else if (e.code == 'wrong-password') {
+        showAlertUserWrongPassword(context);
+      }
     }
     return null;
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,6 +2,7 @@ const String titleApp = "RegEase";
 const String descApp = "RegEase, solusi terbaik\nuntuk mengatasi rasa bosan\nsaat mengantri.";
 const String titleRegister = "Register";
 const String titleLogin = "Login";
+const String titleError = "Error";
 
 const String textWelcome = "Selamat Datang,";
 const String textNext = "Next";
