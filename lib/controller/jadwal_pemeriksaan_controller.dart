210d575a41734f338940fe93b2ba9830637824fb
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2023-07-01 22:06:35 +0700

    admin deleteData jadwal_pemeriksaan_controller

--- lib/controller/jadwal_pemeriksaan_controller.dart
+++ lib/controller/jadwal_pemeriksaan_controller.dart
@@ -3,15 +3,24 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class JadwalPemeriksaanController{
-    final StreamController<List<DocumentSnapshot>> streamController =
+class JadwalPemeriksaanController {
+  final StreamController<List<DocumentSnapshot>> streamController =
       StreamController<List<DocumentSnapshot>>.broadcast();
 
   Stream<List<DocumentSnapshot>> get stream => streamController.stream;
 
-    final Stream<QuerySnapshot> streamAntrianUsers = FirebaseFirestore.instance
+  final Stream<QuerySnapshot> streamAntrianUsers = FirebaseFirestore.instance
       .collection("users")
       .doc(FirebaseAuth.instance.currentUser!.uid)
       .collection("antrian")
       .snapshots();
+
+  Future<void> deleteData(String documentId) async {
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection("antrian")
+        .doc(documentId)
+        .delete();
+  }
 }
