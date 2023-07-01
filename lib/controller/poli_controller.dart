5a2a0155ed59109106423ee1edcca1cc7617c437
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2023-07-01 20:13:24 +0700

    Add addPoli in poli_controller

---
+++ lib/controller/poli_controller.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:aplikasipendaftaranklinik/model/poli_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class PoliController {
+  final poliCollection = FirebaseFirestore.instance.collection('poli');
+
+  final StreamController<List<DocumentSnapshot>> streamController =
+      StreamController<List<DocumentSnapshot>>.broadcast();
+
+  Stream<List<DocumentSnapshot>> get stream => streamController.stream;
+
+  Future<void> addPoli(PoliModel pmmodel) async {
+    final poli = pmmodel.toMap();
+
+    final DocumentReference docref = await poliCollection.add(poli);
+
+    final String docId = docref.id;
+
+    final PoliModel poliModel =
+        PoliModel(uId: docId, namaPoli: pmmodel.namaPoli);
+    await docref.update(poliModel.toMap());
+  }
+}
